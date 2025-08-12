9ee28ecd7ddb8ea1a56845a226d345c2246ce797
Ryu-Eun <ektl4m@gmail.com>
2025-08-12 23:24:03 +0900

    [feature] 커스텀 Exception 추가 - DuplicateUserException - UnauthorizedAccessException - UserNotFoundException

---
+++ test-project/PROJECT-PROMOTION/promotion/user-service/src/main/java/com/practice/userservice/exception/UnauthorizedAccessException.java
@@ -0,0 +1,7 @@
+package com.practice.userservice.exception;
+
+public class UnauthorizedAccessException extends RuntimeException {
+    public UnauthorizedAccessException(String message) {
+        super(message);
+    }
+}

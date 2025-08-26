4cf0cec08684bf39d07e86fa91f4fb511d507f65
Ryu-Eun <ektl4m@gmail.com>
2025-08-27 06:48:51 +0900

    [feature] 신규 모듈 point-service 초기화

---
+++ test-project/PROJECT-PROMOTION/promotion/point-service/src/test/java/com/practice/pointservice/PointServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.pointservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

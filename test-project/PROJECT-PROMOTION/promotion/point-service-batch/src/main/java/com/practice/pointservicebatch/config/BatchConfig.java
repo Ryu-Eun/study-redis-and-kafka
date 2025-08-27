260c55a3b322a97681079b53710f7789a2031fd4
Ryu-Eun <ektl4m@gmail.com>
2025-08-27 09:32:30 +0900

    [feature] BatchConfig 추가

---
+++ test-project/PROJECT-PROMOTION/promotion/point-service-batch/src/main/java/com/practice/pointservicebatch/config/BatchConfig.java
@@ -0,0 +1,13 @@
+package com.practice.pointservicebatch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableBatchProcessing
+@EnableTransactionManagement
+public class BatchConfig {
+
+
+}

a015a491233b55a03e004dc18f9c82125e97a58d
Ryu-Eun <ektl4m@gmail.com>
2025-08-27 13:21:36 +0900

    [build] time-sale-service 모듈 추가 + 루트 프로젝트 settings.gradle에 include

---
+++ test-project/PROJECT-PROMOTION/promotion/time-sale-service/src/test/java/com/practice/timesaleservice/TimeSaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.timesaleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSaleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

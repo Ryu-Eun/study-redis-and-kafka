f2e4bbcb80c3a041a9873d31278bb0625acf5d0c
Ryu-Eun <ektl4m@gmail.com>
2025-08-27 16:41:31 +0900

    [feature] time-sale-service 관련 컨트롤러 추가

---
+++ test-project/PROJECT-PROMOTION/promotion/time-sale-service/src/main/java/com/practice/timesaleservice/controller/v1/ProductController.java
@@ -0,0 +1,43 @@
+package com.practice.timesaleservice.controller.v1;
+
+import com.practice.timesaleservice.dto.ProductDto;
+import com.practice.timesaleservice.entity.Product;
+import com.practice.timesaleservice.service.v1.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductDto.Response> createProduct(@Valid @RequestBody ProductDto.CreateRequest request){
+        Product product = productService.createProduct(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ProductDto.Response.from(product));
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductDto.Response> getProduct(@PathVariable Long productId) {
+        Product product = productService.getProduct(productId);
+        return ResponseEntity.ok(ProductDto.Response.from(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto.Response>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products.stream()
+                .map(ProductDto.Response::from)
+                .collect(Collectors.toList()));
+    }
+
+}
